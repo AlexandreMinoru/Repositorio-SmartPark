3fa8ecf608dc1ba3de419f9b1fa9fa0956d790c6
AlexandreMinoru <alexandreminoru6@gmail.com>
2023-05-30 13:06:58 -0300

    SmartPark-API 2.0

--- API-SmartPark/src/routes.ts
+++ API-SmartPark/src/routes.ts
@@ -3,7 +3,7 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 const router = Router();
 //Rotas USER ------------
-router.post('/smartpark/users',new CreateUserController().handle)
+router.post('/smartpark/userinsert', new CreateUserController().handle)
 
 router.post('/smartpark/login', new AuthUserController().handle)
 export { router };
--- API-SmartPark/src/services/user/AuthUserService.ts
+++ API-SmartPark/src/services/user/AuthUserService.ts
@@ -1,28 +1,28 @@
 import prismaClient from "../../prisma";
 interface AuthRequest {
- email: string;
- password: string;
+    email: string;
+    password: string;
 }
 class AuthUserService {
- async execute({ email, password }: AuthRequest) {
- //verificar se email existe
- const user = await prismaClient.user.findFirst({
- where:{
- email:email
- }
- })
- if (!user){
- throw new Error("Usuario não encontardo")
- }
- //verificar validade password
+    async execute({ email, password }: AuthRequest) {
+        //verificar se email existe
+        const user = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+        if (!user) {
+            throw new Error("Usuario não encontardo")
+        }
+        //verificar validade password
 
- if (user.password!==password){
- throw new Error("Senha errada") 
-}
-//return { ok: "Chamada serviço Auth" }
-return {
-nomeUsuario: "Bem vindo " + user.name
-}
-}
+        if (user.password !== password) {
+            throw new Error("Senha errada")
+        }
+        //return { ok: "Chamada serviço Auth" }
+        return {
+            nomeUsuario: "Bem vindo " + user.name
+        }
+    }
 }
 export { AuthUserService } 
